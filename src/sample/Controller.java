198ca8192b2e8f316646ac8dfbd0d94c60a59e95
Vadim <vasilisk.vv@mail.ru>
2017-07-07 14:38:00 +0300

    Realised "get file path"

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -22,12 +22,17 @@ public class Controller {
 
     }
 
+    @FXML
+    private void getElementPath(){
+        TreeItem<File> selected_file = systemTree.getSelectionModel().getSelectedItem();
+        System.out.println(selected_file.getValue().getPath());
+    }
+
     public void createTree(File dir, TreeItem<File> parent) {
         TreeItem<File> root = new TreeItem<>(dir);
         root.setExpanded(true);
         File[] files = dir.listFiles();
         for (File file : files) {
-
             if (file.isDirectory()) {
                 createTree(file, root);
             } else {
