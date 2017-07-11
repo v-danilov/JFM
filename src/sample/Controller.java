60fd180ce3ab8fc98a2c94cacd783778af6372ff
Vadim <vasilisk.vv@mail.ru>
2017-07-11 20:50:20 +0700

    Icons bind

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,8 +3,11 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Controller {
 
@@ -20,6 +23,9 @@ public class Controller {
     @FXML
     private ListView<File> listView;
 
+    public final Image closedFolder=new Image(ClassLoader.getSystemResourceAsStream("images/closedFolder.png"));
+    public final Image openFolder=new Image(ClassLoader.getSystemResourceAsStream("images/openFolder.png"));
+
 
     @FXML
     public void showFiles(){
@@ -31,42 +37,45 @@ public class Controller {
         createTree(root_directory, null);
         showButton.setVisible(false);
         showButton.setDisable(true);
+
     }
 
     @FXML
     private void getElementPath(){
         listView.getItems().clear();
         TreeItem<File> selected_file = systemTree.getSelectionModel().getSelectedItem();
+
+
         if(selected_file !=null){
             File currentFile = selected_file.getValue();
             listView.getItems().addAll(currentFile.listFiles());
-            //filesTable.getItems().addAll(currentFile.listFiles());
-
-            /*for(File f : currentFile.listFiles()){
-
-                filesTable.getItems().add(f);
-            }*/
-
             System.out.println(selected_file.getValue().getPath());
         }
 
     }
 
     public void createTree(File dir, TreeItem<File> parent) {
-        TreeItem<File> root = new TreeItem<>(dir);
-        root.setExpanded(false);
-        File[] files = dir.listFiles();
-        for (File file : files) {
-            if (file.isDirectory()) {
-                createTree(file, root);
-            }
-        }
 
-        if(parent==null){
-            systemTree.setRoot(root);
-        } else {
-            parent.getChildren().add(root);
-        }
+            TreeItem<File> root = new TreeItem<>(dir);
+            ImageView icon = new ImageView();
+            icon.setImage(closedFolder);
+            icon.setFitHeight(20);
+            icon.setFitWidth(20);
+            root.setGraphic(icon);
+            root.setExpanded(false);
+            File[] files = dir.listFiles();
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    createTree(file, root);
+                }
+            }
+
+            if (parent == null) {
+                systemTree.setRoot(root);
+            } else {
+                parent.getChildren().add(root);
+            }
+
     }
 
 
